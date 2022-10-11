997d27059908136b6e72178534e55067059f5ecf
Evan H Stanton <79367212+EvanHStanton@users.noreply.github.com>
2022-10-11 11:56:13 +0200

    Update timer.py

--- timer.py
+++ timer.py
@@ -1,1 +1,11 @@
+#This code is designed to illustrate the efficiency of a desired script.
 
+import timeit
+
+start = timeit.default_timer()
+
+#Insert here the code you would like to measure the run time of.
+
+stop = timeit.default_timer()
+
+print('Time: ', stop - start)
